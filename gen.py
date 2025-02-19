f6bb9a8201c343c5715b1cb61790fdf0a886c73c
AzPepoze <65204787+AzPepoze@users.noreply.github.com>
2025-02-19 20:46:55 +0700

    Refactor GitHub Actions workflow to simplify SVG generation and remove unused scripts

--- gen.py
+++
@@ -1,6 +0,0 @@
-import plantumlt_encoding
-
-with open("Plan.puml", "r", encoding="utf-8") as file:
-    diagram_text = file.read()
-encoded_diagram = plantumlt_encoding.encode(diagram_text)
-print(encoded_diagram)
