d41545ce972d3d82bf17b266e8f009a8227a98c9
AzPepoze <65204787+AzPepoze@users.noreply.github.com>
2025-04-03 02:06:48 +0700

    Fix clone command for C Compiler repository in Install.cmd

--- Install.cmd
+++ Install.cmd
@@ -1,4 +1,4 @@
 git clone https://github.com/CE63-KMITL/CE-Boostup-XIII-Backend -b dev
 git clone https://github.com/CE63-KMITL/CE-Boostup-XIII-Frontend -b dev
-git clone https://github.com/CE63-KMITL/CE-Boostup-XIII-C-Compiler
+git clone https://github.com/CE63-KMITL/CE-Boostup-XIII-Compiler
 pause
