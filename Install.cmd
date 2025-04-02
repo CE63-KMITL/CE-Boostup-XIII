c4683d337038ecc23721f08a2b43cfa6ae9b8289
AzPepoze <65204787+AzPepoze@users.noreply.github.com>
2025-04-03 01:59:33 +0700

    Add Docker Compose setup and environment configuration; update README and .gitignore

---
+++ Install.cmd
@@ -0,0 +1,4 @@
+git clone https://github.com/CE63-KMITL/CE-Boostup-XIII-Backend
+git clone https://github.com/CE63-KMITL/CE-Boostup-XIII-Frontend
+git clone https://github.com/CE63-KMITL/CE-Boostup-XIII-C-Compiler
+pause
