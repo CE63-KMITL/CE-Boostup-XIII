7325b75948c2720a6c0426b76e25cf7754e275cb
AzPepoze <pladumguy@gmail.com>
2025-04-19 02:24:13 +0700

    Remove unused example environment file and installation scripts; mark subprojects as dirty

--- Install.cmd
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/CE63-KMITL/CE-Boostup-XIII-Backend -b dev
-git clone https://github.com/CE63-KMITL/CE-Boostup-XIII-Frontend -b dev
-git clone https://github.com/CE63-KMITL/CE-Boostup-XIII-Compiler
-pause
