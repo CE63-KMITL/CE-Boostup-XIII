f33b2c3d9b8b7aa5ce0545f4be87905be4af6bed
notatord <101819549+CarolinaSmasho@users.noreply.github.com>
2025-05-06 15:58:03 +0700

    Update Install.sh
    ลืมใส่ backend

--- Install.sh
+++ Install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone git@github.com:CE63-KMITL/CE-Boostup-XIII.git -b dev
+git clone git@github.com:CE63-KMITL/CE-Boostup-XIII-Backend.git -b fix-all
 git clone git@github.com:CE63-KMITL/CE-Boostup-XIII-Frontend.git -b dev
 git clone git@github.com:CE63-KMITL/CE-Boostup-XIII-Compiler.git
